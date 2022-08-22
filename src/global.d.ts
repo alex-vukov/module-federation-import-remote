8b55a5ecd88dca8a6569350ee4a31d23c79b9573
Aleksandar Vukov <aleksandar.vukov@nexo.io>
2022-08-22 15:52:06 +0300

    Configure Build

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+export {};
+
+declare global {
+  let __webpack_init_sharing__: (parameter: string) => Promise<void>;
+  let __webpack_share_scopes__: { default: any };
+}
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+import script from "scriptjs";
+
+export interface ImportRemoteOptions {
+  url: string;
+  scope: string;
+  module: string;
+  remoteEntryFileName?: string | undefined;
+  bustRemoteEntryCache?: boolean | undefined;
+}
+
+const REMOTE_ENTRY_FILE = "remoteEntry.js";
+
+const loadEntryPoint = (
+  url: ImportRemoteOptions["url"],
+  bustRemoteEntryCache: ImportRemoteOptions["bustRemoteEntryCache"],
+) =>
+  new Promise<void>((resolve) => {
+    script.urlArgs(bustRemoteEntryCache ? `t=${new Date().getTime()}` : null); // Add a timestamp to bust the cached remote entry file
+    script(url, () => resolve());
+  });
+
+/* 
+  Dynamically import a remote module using Webpack's loading mechanism:
+  https://webpack.js.org/concepts/module-federation/
+*/
+export const importRemote = async ({
+  url,
+  scope,
+  module,
+  remoteEntryFileName = REMOTE_ENTRY_FILE,
+  bustRemoteEntryCache = true,
+}: ImportRemoteOptions) => {
+  if (!window[scope]) {
+    //Get the remote entry point:
+    await loadEntryPoint(`${url}/{${remoteEntryFileName}}`, bustRemoteEntryCache);
+    if (!window[scope]) {
+      return Promise.reject(new Error(`${scope} could not be located!`));
+    }
+    // Initializes the share scope. This fills it with known provided modules from this build and all remotes
+    await __webpack_init_sharing__("default");
+    // Initialize the container, it may provide shared modules
+    await window[scope].init(__webpack_share_scopes__.default);
+  }
+
+  const moduleFactory = await window[scope].get(module.startsWith("./") ? module : `./${module}`);
+  return moduleFactory();
+};
